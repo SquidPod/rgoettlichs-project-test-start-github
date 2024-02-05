45d9dfcba5c13b0e18020f57978f4bd9ec5684e7
SquidPod <159013962+SquidPod@users.noreply.github.com>
2024-02-05 13:29:56 +0100

    Add files via upload

---
+++ Blinkmorsecodehellowrldrgoettlich.ino
@@ -0,0 +1,122 @@
+
+int wrdlng = 200;
+int shrt = 50;
+int lng = 100;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);                      
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  digitalWrite(LED_BUILTIN, LOW);
+
+  delay(wrdlng);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(wrdlng);
+
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(lng);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(shrt);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+
+  delay(wrdlng);
+
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(lng);
+
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(lng);
+
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(lng);
+
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(shrt); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(shrt);
+
+
+}
